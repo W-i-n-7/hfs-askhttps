62ac8fe430ebca9b3bc03dbeb31dd0c7482551ae
Win_7 <90262580+W-i-n-7@users.noreply.github.com>
2025-02-16 22:30:50 +0200

    fixed
    lol oops

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,5 +1,5 @@
 exports.version = 1.0
 exports.apiRequired = 8.65
-exports.repo = 'W-i-n-7/hfs-askhttps'
+exports.repo = "W-i-n-7/hfs-askhttps"
 exports.description = "Kindly prompts the user to switch to HTTPS when they are using HTTP"
 exports.frontend_js = 'https.js'
