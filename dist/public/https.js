52dfc3cf961bd633e577d0ca6b773b9189107119
Win_7 <90262580+W-i-n-7@users.noreply.github.com>
2025-02-26 17:54:40 +0200

    Update https.js
    Updated for new translation

--- dist/public/https.js
+++ dist/public/https.js
@@ -1,6 +1,16 @@
 if (window.location.protocol === 'http:') {
     const { t } = HFS
-    if (confirm(t('switchtohttps', 'This page is currently loaded over HTTP. Do you want to switch to HTTPS?'))) {
+    const conf = HFS.getPluginConfig()
+    let redirect
+    if (conf['moreInfo'])
+    {
+        redirect = confirm(t('switchtohttps', 'This page is currently loaded over HTTP. Do you want to switch to HTTPS?') + '\n' + t('continueanyway', 'You may need to click "More info" and a small link "Continue anyway"'))
+    }
+    else
+    {
+        redirect = confirm(t('switchtohttps', 'This page is currently loaded over HTTP. Do you want to switch to HTTPS?'))
+    }
+    if (redirect) {
         window.location.href = window.location.href.replace('http:', 'https:');
     }
 }
